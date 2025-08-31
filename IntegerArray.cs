af3411afa0d8da073fefb71efc77e545b275eed9
Jaya Lakshmi Tangirala <jt3951@hallam.shu.ac.uk>
2025-08-31 12:59:53 +0100

    Add project files.

---
+++ IntegerArray.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayADT
+{
+    internal class IntegerArray
+    {
+        private int[] A;
+        private int maxLength;
+        private int length;
+        public IntegerArray(int n)
+        {
+            maxLength = n;
+            A = new int[maxLength];
+            length = 0;
+        }
+
+        public void traverse()
+        {
+            if (length == 0)
+                Console.WriteLine("Array is empty");
+            else
+            {
+                Console.WriteLine("Elements in Array are: ");
+                foreach (int element in A)
+                    Console.Write(element + "\t");
+                Console.WriteLine();
+            }
+        }
+
+        public void insert(int index, int element)
+        {
+            if (index < 0 || index > length)
+            {
+                Console.WriteLine("Invalid index.");
+                return;
+            }
+
+            if (length == maxLength)
+            {
+                Console.WriteLine("Cant insert the element, No space available in the array");
+                return;
+            }
+            else
+            {
+                for (int i = length; i > index; i--)
+                    A[i] = A[i - 1];
+                A[index] = element;
+                length++;
+                Console.WriteLine(element + " is inserted at "+index+ " successfully");
+            }
+        }
+        public int search(int element)
+        {
+            return (Array.IndexOf(A, element));
+        }
+        public void delete(int element)
+        {
+            int index = Array.IndexOf(A, element);
+            if (index == -1)
+            {
+                Console.WriteLine(element +" not found!");
+                return;
+            }
+            for (int i = index; i < length; i++)
+                A[i] = A[i + 1];
+            length--;
+            Console.WriteLine(element + " deleted successfully!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ArrayADT
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            IntegerArray IA = new IntegerArray(9);
+            IA.insert(0,56); IA.insert(1,49); IA.insert(2,67); 
+            IA.insert(3,21); IA.insert(4,90);
+
+            IA.traverse();
+
+            IA.insert(1, 70);
+            IA.traverse();
+
+            int index=IA.search(67);
+            if (index == -1)
+                Console.WriteLine(67 + " not found!");
+            else
+                Console.WriteLine(67 + " found at position " + index);
+            index=IA.search(10);
+            if (index == -1)
+                Console.WriteLine(10 + " not found!");
+            else
+                Console.WriteLine(10 + "found at position " + index);
+
+            IA.delete(49); IA.traverse();
+            IA.delete(10);
+        }
+    }
+}
